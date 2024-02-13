74c02216a6c3c121c551a2daf070091030d2caef
AR Aourangazeb Alif <ar.aourangazeb.alif@gmail.com>
2024-02-13 23:49:37 +0600

    rectangle area done

--- areaCalculator.js
+++ areaCalculator.js
@@ -1,14 +1,27 @@
-function triangleArea(){
-    const triangleHeight = Number(document.getElementById("triangle-height").value);
-    const triangleBase = Number(document.getElementById("triangle-base").value);
+function Area(shape){
+    const x = Number(document.getElementById(shape+"-x").value);
+    const y = Number(document.getElementById(shape+"-y").value);
+    let area;
+    let formula;
+    let ShapeMsg;
 
-    const area = 0.5 * triangleBase * triangleHeight;
+    if(shape === "triangle"){
+        area = 0.5 * y * x;
+        formula = `0.5 X ${x} (b) X ${y} (h)`
+        ShapeMsg = `Triangle`;
     
+    }else if(shape === "rectangle"){
+        area = x * y;
+        formula = `${x} (w) X ${y} (l)`;
+        ShapeMsg = `Rectangle`;
+    
+        
+    }
     document.getElementById("result").innerHTML = `
-    <span class='text-3xl font-bold text-gray-800 mb-3'>Triangle Area:</span>
-    <span class = 'text-gray-600 mb-2'>0.5 X ${triangleBase} (b) X ${triangleHeight} (h)</span>
-    <span class = 'text-red-500 text-2xl font-semibold'>${area}</span>
-    `
+        <span class='text-3xl font-bold text-gray-800 mb-3'>${ShapeMsg} Area:</span>
+        <span class = 'text-gray-600 mb-2'>${formula}</span>
+        <span class = 'text-red-500 text-2xl font-semibold'>${area}</span>
+        `
 }
 
 
